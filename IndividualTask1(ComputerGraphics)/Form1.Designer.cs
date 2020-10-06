560f85a09987ec4207d2efec19db0bfc15f408e7
ivolnoboi <irinavolnoboy@gmail.com>
2020-10-06 20:51:44 +0300

    Added drawing points and clear

--- IndividualTask1(ComputerGraphics)/Form1.Designer.cs
+++ IndividualTask1(ComputerGraphics)/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(221, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(889, 635);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите точки";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(18, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить выпуклую оболочку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(18, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1122, 659);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Построение выпуклой оболочки методом заворачивания подарка (Джарвиса)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- IndividualTask1(ComputerGraphics)/Form1.cs
+++ IndividualTask1(ComputerGraphics)/Form1.cs
@@ -12,9 +12,41 @@ namespace IndividualTask1_ComputerGraphics_
 {
     public partial class Form1 : Form
     {
+        LinkedList<PointF> points;
+        Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            points = new LinkedList<PointF>();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Jarvis();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SolidBrush solidBrush = new SolidBrush(Color.Red);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillEllipse(solidBrush, e.X, e.Y, 3, 3);
+            g.DrawEllipse(new Pen(Color.Red), e.X, e.Y, 3, 3);
+            pictureBox1.Image = bmp;
+            points.AddLast(e.Location);
+        }
+
+        private void Jarvis()
+        {
+
         }
     }
 }
